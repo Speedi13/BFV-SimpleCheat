570dadc2889cb9bbc411da34e752c9e4f6b4a8bb
Speedi13 <Speedi13_im_web@web.de>
2019-01-02 10:56:44 +0100

    Update CustomOcclusionQueryManager.cpp

--- BFV-SimpleCheat/CustomOcclusionQueryManager.cpp
+++ BFV-SimpleCheat/CustomOcclusionQueryManager.cpp
@@ -1,7 +1,7 @@
 #include "CustomOcclusionQueryManager.h"
 
 t_fb__WorldOcclusionQueryRenderModule__drawBatchQuery 			fb__WorldOcclusionQueryRenderModule__drawBatchQuery = 			(t_fb__WorldOcclusionQueryRenderModule__drawBatchQuery)			0x141DDAFC0; //4C 8B CD 4C 8B C7 48 8B D6 49 8B CE E8 ?? ?? ?? ?? EB 10
-t_fb__WorldOcclusionQueryRenderModule__retrieveBatchQueryData 	fb__WorldOcclusionQueryRenderModule__retrieveBatchQueryData = 	(t_fb__WorldOcclusionQueryRenderModule__retrieveBatchQueryData)	0x1491CB270; //pattern from above minus two
+t_fb__WorldOcclusionQueryRenderModule__retrieveBatchQueryData 		fb__WorldOcclusionQueryRenderModule__retrieveBatchQueryData = 	(t_fb__WorldOcclusionQueryRenderModule__retrieveBatchQueryData)	0x1491CB270; //pattern from above minus two
 t_fb__WorldOcclusionQueryRenderModule__processBatchQueries 		fb__WorldOcclusionQueryRenderModule__processBatchQueries = 		(t_fb__WorldOcclusionQueryRenderModule__processBatchQueries)	0x141DEF5E0; //00 00 C6 44 ?? ?? ?? 41 B1 01 49 8B D4 E8
 t_fb__WorldOcclusionQueryRenderModule__insertBatchQuery 		fb__WorldOcclusionQueryRenderModule__insertBatchQuery = 		(t_fb__WorldOcclusionQueryRenderModule__insertBatchQuery)		0x141DE9720;
 
